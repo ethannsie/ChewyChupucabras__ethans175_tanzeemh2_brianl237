8b6358158c08e650325a78062bf0ed40a2455e83
ethannsie <esie50@stuy.edu>
2025-01-04 22:32:35 -0500

    fixed some bugs and added flash messages

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ DB_FILE = "db.py"
 app = Flask(__name__)
 
 app.secret_key = os.urandom(32)
+active_sessions = {}
 
 if (not os.path.isfile("chupaPokemon.db")):
     db.setup() # sets up databases
@@ -24,28 +25,51 @@ if (not os.path.isfile("chupaPokemon.db")):
 @app.route("/", methods=['GET', 'POST'])
 def home():
     passValue = 'username' in session
-    print(passValue)
+    print(active_sessions)
+    if 'username' in session:
+        passUsers = {}
+        for user in active_sessions:
+            if user != session['username']:
+                passUsers[user] = db.getUserID(session['username'])
+        return render_template("home.html", logged_in = passValue, username=session['username'], activeUsers=passUsers)
     return render_template("home.html", logged_in = passValue)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     username = request.form['username']
     password = request.form['password']
-    db.addUser(username, password)
-    return redirect('/')
+
+    if db.getUserID(username) >= 0:
+        flash("Username already exists", 'error')
+        return redirect("/")
+    else:
+        session['username'] = username
+        flash("Registered Sucessfully!", "success")
+        db.addUser(username, password)
+        return redirect("/")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
-    session['username'] = username
-    if 'username' in session:
+
+    if username in active_sessions:
+        flash("You already have an active session.", 'error')
+    elif db.getUserID(username) >= 0:
+        session['username'] = username
+        active_sessions[session['username']] = db.getUserID(session['username'])
         db.updateLoginTime(session['username'])
-    return redirect('/')
+        flash("Logged in", 'success')
+    else:
+        flash("Incorrect username or password.", 'error')
+    return redirect("/")
 
 @app.route("/logout")
 def logout():
-    session.pop('username', None)
+    if 'username' in session:
+        flash("Logged out", 'success')
+        active_sessions.pop(session['username'])
+        session.pop('username', None)
     return redirect("/")
 
 @app.route("/chupadex")
