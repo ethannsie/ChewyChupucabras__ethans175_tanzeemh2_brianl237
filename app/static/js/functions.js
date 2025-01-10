b08e93ca8bb2392558fe25041c98da0585e30a4f
Tanzeem Hasan <thasan50@stuy.edu>
2025-01-10 10:41:37 -0500

    Account settings back to working

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -1,6 +1,6 @@
-var toggleHiddden = function(elementid) {
+var toggleHidden = function(id) {
   console.log("toggling hidden");
-  const toggle = document.getElementById(elementid);
+  const toggle = document.getElementById(id);
   toggle.classList.toggle('hidden');
 }
 var bar = function(n) {
